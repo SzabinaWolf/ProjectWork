ecc91010db8ad23ecac0646ba873a53382f15d5c
Szabina Wolf <drwolfszabinajudit@gmail.com>
2023-10-04 00:37:51 +0200

    first commit

---
+++ src/assets/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyB_VXXReHmoridQCqE3Q1lj9rchDzvCXAU",
+        authDomain: "szabina-blog-7da22.firebaseapp.com",
+        projectId: "szabina-blog-7da22",
+        storageBucket: "szabina-blog-7da22.appspot.com",
+        messagingSenderId: "377530243167",
+        appId: "1:377530243167:web:4fc19b72019af35ff0a3ea"
+    }
+
+};
